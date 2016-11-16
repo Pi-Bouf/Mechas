6429f2fae0ef52602ecd2b8d352ebbc58d76b31d
Pierre Bouffier <pierre.bouffier05@gmail.com>
2016-11-16 16:52:26 +0100

    Modification client.

--- Socket_Client/script.js
+++ Socket_Client/script.js
@@ -1,15 +1,15 @@
-function Game() {
+window.onload = function() {
 
     var socket = io.connect('http://localhost:357');
-    socket.emit('security', "Zorrooooo|111111");
-    socket.emit("test", "ok");
+
     var player = document.getElementById("player1");
     var position = player.getBoundingClientRect();
     var x = position.left;
-    var y = position.y;
-}
+    var y = position.top;
 
-function test() {
-    console.log(x);
-    socket.emit("position", "ok");
+    player.onclick = function(e) {
+        var mouseX = e.pageX;
+        var mouseY = e.pageY;
+    }
+
 }
